26a68acc01ef079862e42bfead79abfec6064a45
TW2 <util2@msn.com>
2026-02-16 20:41:00 +0100

    Check pass

--- src/main/java/loli/ui/control/mtable/MTable.java
+++ src/main/java/loli/ui/control/mtable/MTable.java
@@ -1,9 +1,17 @@
 package loli.ui.control.mtable;
 
 import javax.swing.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MTable extends JPanel {
     private final List<Voyager> voyagers;
 
+    public MTable() {
+        voyagers = new ArrayList<>();
+    }
+
+    public List<Voyager> getVoyagers() {
+        return voyagers;
+    }
 }
